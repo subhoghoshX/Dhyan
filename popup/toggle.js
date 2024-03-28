a5951e156368d195117138d137dd578f264babb5
Subho <contact.subhoghosh@gmail.com>
2024-03-28 20:40:32 +0530

    revamp popup & toggles with new design

--- popup/toggle.js
+++ popup/toggle.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   function createToggles(toggleInfo) {
     const labelElem = document.createElement("label");
+    labelElem.className = "flex items-center gap-3";
 
     labelElem.addEventListener("click", (e) => {
       if(e.target.tagName === "LABEL") return;
@@ -33,12 +34,18 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     if(toggleInfo.isChecked) {
       labelElem.innerHTML = `
-        <input type="checkbox" checked />
+        <input type="checkbox" checked class="[&:checked+div]:bg-indigo-600 [&:checked+div>span]:bg-white [&:checked+div>span]:inset-[3px_3px_auto_23px] [&:focus-visible+div]:outline sr-only" />
+        <div class="w-10 h-5 rounded-full relative outline-offset-2 outline-indigo-600 outline-2 bg-gray-200">
+          <span class="w-3.5 h-3.5 bg-white inline-block rounded-full absolute inset-[3px_23px_auto_3px] transition-[inset] ease-in-out"></span>
+        </div>
         ${toggleInfo.text}
       `
     } else {
       labelElem.innerHTML = `
-        <input type="checkbox" />
+        <input type="checkbox" class="[&:checked+div]:bg-indigo-600 [&:checked+div>span]:bg-white [&:checked+div>span]:inset-[3px_3px_auto_23px] [&:focus-visible+div]:outline sr-only" />
+        <div class="w-10 h-5 rounded-full relative outline-offset-2 outline-indigo-600 outline-2 bg-gray-200">
+          <span class="w-3.5 h-3.5 bg-white inline-block rounded-full absolute inset-[3px_23px_auto_3px] transition-[inset] ease-in-out"></span>
+        </div>
         ${toggleInfo.text}
       `
     }
